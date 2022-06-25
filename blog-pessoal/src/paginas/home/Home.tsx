47eceae5724aedb5005aeae5ca4be18b69e2891e
isabelapiana <isabelapiana91@gmail.com>
2022-06-24 22:44:01 -0300

    Desenvolvendo blog pessoal com React

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://www.mediacaonline.com/blog/wp-content/uploads/2021/05/img12.jpg" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
