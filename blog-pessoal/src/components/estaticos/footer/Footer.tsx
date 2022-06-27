6b72d3418b00519a1f974f0a74542398fdc5194c
isabelapiana <isabelapiana91@gmail.com>
2022-06-27 14:36:10 -0300

    Trabalhando com React

--- blog-pessoal/src/components/estaticos/footer/Footer.tsx
+++ blog-pessoal/src/components/estaticos/footer/Footer.tsx
@@ -19,7 +19,7 @@ function Footer(){
                     </Box>
                     <Box style={{ backgroundColor: "#975261", height: "60px" }}>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2022 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://github.com/isabelapiana/React-Blog-Pessoal">
